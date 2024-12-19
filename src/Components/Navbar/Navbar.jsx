0e8f794f4add469f707ce4a676f484e41c1f1e22
Chandan Pandey <133705046+angry0batman@users.noreply.github.com>
2024-12-19 10:37:27 +0530

    nav bar added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
     <nav>
-        <img src={logo} alt="" />
+        <img src={logo} alt="" className='logo' />
         <ul>
             <li><a href="#">Home</a></li>
             <li><a href="#">About us</a></li>
